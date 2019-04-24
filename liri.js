dd165609fa48deed6a5fe99d03424c3afc67ef40
Robert Butler <robwbutler@gmail.com>
2019-04-24 01:55:06 -0400

    concert this response

--- liri.js
+++ liri.js
@@ -1,5 +1,34 @@
 require("dotenv").config();
+var axios = require("axios")
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var artist = process.argv.splice(3, process.argv.length - 1).join('+');
+console.log(artist)
+
+if (command ==="concert-this"){
+    queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    axios
+    .get(queryUrl)
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+    })
+}
