c3defcb067d5b990cece5f04021804158f2f11fa
Robert Butler <robwbutler@gmail.com>
2019-04-24 02:27:10 -0400

    omdb response done

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var keys = require("./keys.js");
 
 var command = process.argv[2];
 var work = process.argv.splice(3, process.argv.length - 1).join('+');
-console.log(work)
+
 
 if (command === "concert-this"){
     queryUrl = "https://rest.bandsintown.com/artists/" + work + "/events?app_id=codingbootcamp"
@@ -43,3 +43,40 @@ if (command === "concert-this"){
         console.log(error.config);
     })
 };
+
+
+if (command === "movie-this"){
+  var queryUrl = "http://www.omdbapi.com/?t=" + work + "&y=&plot=short&apikey=trilogy";
+
+  axios
+.get(queryUrl)
+.then(function(response) { 
+    var data = response.data;
+    console.log(data.Title);
+    console.log("Released in " + data.Year);
+    console.log("IMDB rating: " + data.Ratings[0].Value);
+    console.log("RT rating: " + data.Ratings[1].Value);
+    console.log("Country: " + data.Country);
+    console.log("Language: " + data.Language);
+    console.log("Plot: " + data.Plot);
+    console.log("Actors: " + data.Actors);
+
+})
+.catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+})
+}
